92f28dceb2bf4f34b6beb58fa0ee9f794a2c3a67
luisf <luisf@DESKTOP-P0D3UQL>
2023-04-05 18:58:44 -0700

    Initial commit

--- src/demo2/practice2.java
+++ src/demo2/practice2.java
@@ -3,6 +3,6 @@ package demo2;
 public class practice2 {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("GetHub Lecture...");
+		System.out.println("GetHub Lecture 2...");
 	}
 }
